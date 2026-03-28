7655de2e01978c7bd556af633d49f494a9761245
Strappazzon <im@strappazzon.xyz>
2026-03-28 21:14:52 +0100

    Handle `CTRL+Q`
    https://github.com/Snootiful/mint-personal/commit/43ed5618bddc95b485c699b901301a3ee7f499da

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1953,6 +1953,11 @@ impl eframe::App for App {
                                 self.scroll_to_match = true;
                             }
                         }
+                        egui::Event::Key { key, modifiers, pressed, .. } => {
+                            if key == &egui::Key::Q && *pressed && modifiers.ctrl {
+                                std::process::exit(0);
+                            }
+                        }
                         _ => {}
                     }
                 }
